a9c169532b7bd4c54b8e2b0d88902ae2554e8108
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-04-25 20:24:10 +0300

    first commit

---
+++ practice.py
@@ -0,0 +1,24 @@
+##Declare a list with more than 5
+b=['mango','orange','pineapple','apple','coconut','banana']
+print(b)
+#Find the lenght of your listixed
+print(len(b))
+
+#Get the first item, the middle item and the last item of the list
+b="mango"(middle)("banana")
+print(b)
+#Declare a list called mixed_data_types, put your(name, , height, marital status, address)age
+mixed_data_types=("yvonne",34,"single","740 SIAYA",18)
+print(mixed_data_types)
+#Display the sum of 5+10 using two variables x and y
+x=4
+y=13
+print(x+y)
+
+#using the lenght method to print the length of string
+x="Hello World"
+print(len(x))
+
+#print the second item in the fruit list
+fruits=["apple","banana","cherry"]
+print(fruits[1])
